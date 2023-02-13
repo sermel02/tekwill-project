f55432d98c557e96b637f2dcb8f289544f1db2ba
Serghei <sermel@MacBook-Air-Sergej.local>
2023-02-13 19:38:00 +0200

    added dialog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SwiperModule } from 'swiper/angular';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { HttpClientModule } from '@angular/common/http';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CategoriesComponent } from './components/categories/categories.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { OrderComponent } from './components/order/order.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
 
 
 @NgModule({
@@ -38,6 +40,7 @@ import { OrderComponent } from './components/order/order.component';
     ProductsComponent,
     ContactComponent,
     OrderComponent,
+    LoginDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -51,7 +54,8 @@ import { OrderComponent } from './components/order/order.component';
     HttpClientModule,
     MatBadgeModule,
     MatSnackBarModule,
-    MatCardModule
+    MatCardModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
+import { LoginDialogComponent } from './../login-dialog/login-dialog.component';
 import { CartService } from './../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
@@ -10,13 +12,23 @@ export class HeaderComponent implements OnInit {
 
   public totalItems: number = 0;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.cartService.getProducts().
     subscribe(res => {
       this.totalItems = res.length;
     })
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(LoginDialogComponent, {
+      width: '300px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
